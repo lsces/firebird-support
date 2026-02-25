e80331e5ef449883305d5a2ba26bb1c0c467f881
lsces <lester@lsces.co.uk>
2026-02-25 13:03:50 +0000

    Use the returned id value in place of the pdo::lastInsertID which firebird does not support

--- src/Query/Processors/FirebirdProcessor.php
+++ src/Query/Processors/FirebirdProcessor.php
@@ -3,6 +3,7 @@
 namespace Xgrz\Firebird\Query\Processors;
 
 use Illuminate\Database\Query\Processors\Processor;
+use \Illuminate\Database\Query\Builder;
 
 class FirebirdProcessor extends Processor
 {
@@ -17,5 +18,23 @@ class FirebirdProcessor extends Processor
         return array_map(function ($result) {
             return ((object) $result)->column_name;
         }, $results);
+    }
+
+    /**
+     * Process an  "insert get ID" query.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @param  string  $sql
+     * @param  array  $values
+     * @param  string|null  $sequence
+     * @return int
+     */
+    public function processInsertGetId(Builder $query, $sql, $values, $sequence = null)
+    {
+        $query->getConnection()->insert($sql, $values);
+
+      	$id = $query->getConnection()->getLastInsertId();
+
+        return is_numeric($id) ? (int) $id : $id;
     }
 }
